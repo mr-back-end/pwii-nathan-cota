88510a18499c50fe06da04ab4c23a0be86ebf4da
Nathan Cota <nathan.hpc2008@gmail.com>
2025-03-25 16:31:16 -0300

    aula sobre concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,17 @@
+<?php
+
+ECHO "Hello World";
+echo "Hello World";
+EcHo "Hello World";
+
+$nome = "Nathan";
+$sobrenome = "Cota";
+$numero = 4;
+
+echo $nome . " lindo" . "<br>";
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . " <p> " . $nome .
+" " . $sobrenome . "</p>";
+?>
+
+
